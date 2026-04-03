3ab2691baf609c0398e782fb1e409cf7fc30ba8a
kaiserraf <rafaelmachado2507oliveira@hotmail.com>
2026-04-03 00:59:23 -0300

    limdo

---
+++ README.md
@@ -0,0 +1,1 @@
+# parei em QueryString
--- src/controllers/podcastsController.ts
+++ src/controllers/podcastsController.ts
@@ -11,7 +11,7 @@ export const getListEps = async (req:IncomingMessage, res:ServerResponse) => {
 };
 
 export const getFilterEps = async (req:IncomingMessage, res:ServerResponse) => {
-    const content = await serverFilterEp("PodPah");
+    const content = await serverFilterEp("Flow Podcast");
 
     res.writeHead(200, {'content-type': "application/json"});
     res.end(JSON.stringify(content)); // stringfy = muda de json pra texto
